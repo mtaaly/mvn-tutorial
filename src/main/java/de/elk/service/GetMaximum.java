4d3a5e377b43d28b5a2d89452fe98f4d023417f7
vigadacl <nkemegnivianney@yahoo.fr>
2021-06-03 18:28:55 +0200

    sssfsdsdfsdf

--- src/main/java/de/elk/service/GetMaximum.java
+++ src/main/java/de/elk/service/GetMaximum.java
@@ -6,7 +6,7 @@ public class GetMaximum {
         public int getMaximum(int[] arr) {
             int max = arr[0];
             for(int i = 0; i < arr.length ; i++) {
-                if(arr[i]>max) {
+                if(arr[i]<max) {
                     max = arr[i];
                 }
             }
